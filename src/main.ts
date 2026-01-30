472de57f6bdd33f163a33593d69959a918a03229
julien <julien.faure@iaca-electronique.com>
2026-01-30 22:50:29 +0100

    Use configurable screen size and update display mode in engine setup

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { Color, DisplayMode, Engine, FadeInOut } from "excalibur";
 import { loader } from "./resources";
 import { MyLevel } from "./level";
+import {SCREEN_SIZE} from "./config/Settings";
 
 // Goal is to keep main.ts small and just enough to configure the engine
 
 const game = new Engine({
-  width: 800, // Logical width and height in game pixels
-  height: 600,
-  displayMode: DisplayMode.FitScreenAndFill, // Display mode tells excalibur how to fill the window
+  width: SCREEN_SIZE.width, // Logical width and height in game pixels
+  height: SCREEN_SIZE.height,
+  displayMode: DisplayMode.FitScreenAndZoom, // Display mode tells excalibur how to fill the window
   pixelArt: true, // pixelArt will turn on the correct settings to render pixel art without jaggies or shimmering artifacts
   scenes: {
     start: MyLevel
