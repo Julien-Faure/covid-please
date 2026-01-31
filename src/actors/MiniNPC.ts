69deb58469606c287c53370ccad2eedc46771b21
julien <julien.faure@iaca-electronique.com>
2026-01-31 11:23:31 +0100

    Add `MiniNPC` actor and `MiniNPCFactory` for creating dynamic NPCs

---
+++ src/actors/MiniNPC.ts
@@ -0,0 +1,22 @@
+import * as ex from "excalibur";
+
+export class MiniNPC extends ex.Actor {
+
+    constructor(pos: ex.Vector, direction: boolean, speed: number) {
+
+        const directionFactor = direction ? -1 : 1;
+        super({
+            pos,
+            anchor: ex.vec(0, 0),
+            width: 10,
+            height: 10,
+            color: ex.Color.fromHex('#4500f3'),
+            vel: ex.vec(directionFactor * speed, 0),
+            z: 2
+        });
+
+        this.on('exitviewport', () => this.kill());
+    }
+
+
+}
