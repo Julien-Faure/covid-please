64035b70f5bd57dd9d9f8fb42a30b357f72cc8e7
julien <julien.faure@iaca-electronique.com>
2026-02-01 17:10:12 +0100

    Add `ConvocationDisruptor`: handle name/date/location disruptions; update `ContextMasterDisruptor` to include new disruptor.

--- src/services/ContextMasterDisruptor.ts
+++ src/services/ContextMasterDisruptor.ts
@@ -6,6 +6,7 @@ import ContextDisruptor from "./ContextDisruptor";
 import {IdCardDisruptor} from "./distrubtors/IdCardDisruptor";
 import {AttestationDisruptor} from "./distrubtors/AttestationDisruptor";
 import {WorkCertDisruptor} from "./distrubtors/WorkCertDisruptor";
+import {ConvocationDisruptor} from "./distrubtors/ConvocationDisruptor";
 
 
 export class ContextMasterDisruptor {
@@ -14,10 +15,14 @@ export class ContextMasterDisruptor {
     constructor() {
         this.disruptors = [];
 
+        // GLOBALLY DISRUPTORS
         this.disruptors.push(new FakeSignatureDisruptor(0.02));
         this.disruptors.push(new IdCardDisruptor(0.05, 0.05));
         this.disruptors.push(new AttestationDisruptor(0.08, 0.07, 0.1));
+
+        // SPECIFIC DISRUPTORS
         this.disruptors.push(new WorkCertDisruptor(0.2, 0.1));
+        this.disruptors.push(new ConvocationDisruptor(0.06, 0.1, 0.05));
     }
 
     disturb(context: Context): FinalContext {
